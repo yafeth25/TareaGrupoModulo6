8364cc35c3fcccb338b44bb600cb8445d0965f77
yafeth25 <57696912+yafeth25@users.noreply.github.com>
2019-11-16 17:51:25 -0600

    Create TareaGrupoModulo6.java

---
+++ TareaGrupoModulo6.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TareaGrupo;
+
+/**
+ *
+ * @author Walter Sanchez
+ */
+
+   public class Persona
+{
+     private int id;
+	private String Nombre;
+	private String Apellidos;
+	private int Edad;
+  
+   class Doctor extends persona {
+		
+	}
+
+      class Deportista extends {
+		
+	}
